276a725cba61d0e595bf1c36e0089b3dff0919b1
Lukas Yeung <44145664+LukasY98@users.noreply.github.com>
2025-03-13 16:38:03 +0100

    Functions to calculate integral and derivative for PID algorithm added

--- neat_wluff1.ino
+++ neat_wluff1.ino
@@ -1,3 +1,7 @@
+// Constants
+const int MAX_SENSOR_READING = 1023; //TODO: Adjust for real world values
+
+
 // Variables
 int sensorReadingFront = 0;
 int sensorReadingL1 = 0;
@@ -8,6 +12,16 @@ int sensorReadingBack = 0;
 
 int highestIndex = 0;
 int highestValue = 0;
+
+int integral = 0;
+int lastError = 0;
+
+float previousTime = 0.0;
+
+float Kp = 1.0;
+float Ki = 0.5;
+float Kd = 0.2;
+
 
 // Helper Functions
 void readSensorData()
@@ -39,21 +53,17 @@ int calcError(int sensorReadings[])
     // Front sensor has the highest value
     if (sensorReadings[1] > sensorReadings[2]) // Light source is slightly left of the front sensor
     {
-      int diff = 0;
-      diff = sensorReadings[1] - sensorReadings[2];
-      int error = 0;
-      error = map(diff, 0, 1023, 0, 100);
+      int diff = sensorReadings[1] - sensorReadings[2];
+      int error = map(diff, 0, MAX_SENSOR_READING, 0, 100);
       return error;
     }
     if (sensorReadings[2] > sensorReadings[1]) // Light source is slightly right of the front sensor
     {
-      int diff = 0;
-      diff = sensorReadings[2] - sensorReadings[1];
-      int error = 0;
-      error = map(diff, 0, 1023, 0, -100);
+      int diff = sensorReadings[2] - sensorReadings[1];
+      int error = map(diff, 0, MAX_SENSOR_READING, 0, -100);
       return error;
     }
-    else
+    else // Light source is exactly in the middle between sensor L1 and R1
     {
       return 0;
     }
@@ -71,21 +81,42 @@ int calcError(int sensorReadings[])
     return -200;
   case 5:
     // Back sensor has the highest value
-    if (sensorReadings[3] > sensorReadings[4])
+    if (sensorReadings[3] > sensorReadings[4]) //Light source is slightly left of the back sensor
     {
       return 300;
     }
-    else
+    else //Light source is slightly right of the back sensor
     {
       return -300;
     }
   default:
     Serial.println("Unknown sensor index");
     return 0;
-}
+  }
 }
 
-void printSensorData()
+void calcIntegral(int error)
+{
+  // If the sign (+/-) of the error changes or the error is 0, reset the integral
+  if ((error > 0) & (integral < 0) | (error < 0) & (integral > 0) | error == 0)
+  {
+    integral = 0;
+  }
+  integral += error;
+}
+
+float calcDerivative(int error)
+{
+  float currentTime = millis();
+  float deltaTime = (currentTime - previousTime) / 1000.0; // Convert to seconds
+  previousTime = currentTime;
+  if (deltaTime > 0)
+  {
+    return (error - lastError) / deltaTime;
+  }
+}
+
+void printData(int error, float derivative, float output)
 {
   Serial.print("Sensor Front: ");
   Serial.print(sensorReadingFront);
@@ -104,10 +135,19 @@ void printSensorData()
   Serial.print(" (Sensor: ");
   Serial.print(highestIndex);
   Serial.println(")");
+  Serial.print("Error: ");
+  Serial.print(error);
+  Serial.print(" Integral: ");
+  Serial.print(integral);
+  Serial.print(" Derivative: ");
+  Serial.println(derivative);
+  Serial.print("PID: ");
+  Serial.println(output);
 }
 
 
 
+// Main Functions
 void setup()
 {
   pinMode(A0, INPUT);
@@ -126,10 +166,14 @@ void loop()
   
   findHighestValue(sensorReadings);
   
-  printSensorData();
-  
   int error = calcError(sensorReadings);
-  Serial.println(error);
+  calcIntegral(error);
+  float derivative = calcDerivative(error);
 
-  delay(1000);
+  float output = (Kp * error) + (Ki * integral) + (Kd * derivative);
+  
+  printData(error, derivative, output);
+  
+  lastError = error;
+  delay(4000);
 }
